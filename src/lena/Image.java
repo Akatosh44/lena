dab8046a5921339f375188d01fecc26b37c5dadb
ebriand <ebriand@pc-b13-15.eleves.ecn>
2014-12-01 10:38:25 +0100

    Classe Image JavaDoc + toString

--- src/lena/Image.java
+++ src/lena/Image.java
@@ -17,20 +17,32 @@ public class Image {
     private int height;
     private ArrayList<Pixel> Pixels;
 
+    /**
+     * Empty Constructor Image
+     */
     public Image() {
         this.name="Default";
         this.width = 10;
         this.height = 10;
         Pixels = new ArrayList<Pixel>();
     }
-    
+    /**
+     * Constructor with name
+     * @param name 
+     */
     public Image(String name) {
+        this();
         this.name= name;
-        this.width = 10;
-        this.height = 10;
-        Pixels = new ArrayList<Pixel>();
+        
     }
     
+    /**
+     * Ful Constructor
+     * @param name
+     * @param width
+     * @param height
+     * @param pix 
+     */
     public Image(String name, int width, int height, ArrayList<Pixel> pix) {
         this.name= name;
         this.width = width;
@@ -38,37 +50,74 @@ public class Image {
         Pixels = pix;
     }
         
-        
+    /**
+     * Getter Name
+     * @return 
+     */   
     public String getName() {
         return name;
     }
-
+    /**
+     * Setter Name
+     * @param name 
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    
+    /**
+     * Getter Width
+     * @return 
+     */
     public int getWidth() {
         return width;
     }
-
+    /**
+     * Setter Width
+     * @param width 
+     */
     public void setWidth(int width) {
         this.width = width;
     }
-
+    
+    /**
+     * Getter Height
+     * @return 
+     */
     public int getHeight() {
         return height;
     }
-
+    
+    /**
+     * Setter Height
+     * @param height 
+     */
     public void setHeight(int height) {
         this.height = height;
     }
-
+    
+    /**
+     * Getter Pixels
+     * @return 
+     */
     public ArrayList<Pixel> getPixels() {
         return Pixels;
     }
-
+    
+    /**
+     * Setter Pixels
+     * @param Pixels 
+     */
     public void setPixels(ArrayList<Pixel> Pixels) {
         this.Pixels = Pixels;
+    }
+    /**
+     * toString
+     * @return 
+     */
+    @Override
+    public String toString(){
+        return this.name;
     }
     
     
