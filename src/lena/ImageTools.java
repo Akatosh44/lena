1c8b7212e05f350925216f109ebeaf7c7afb7790
avinesse <avinesse@pc-b13-10.eleves.ecn>
2014-12-01 10:53:58 +0100

    Class ImageTools

---
+++ src/lena/ImageTools.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lena;
+
+/**
+ *
+ * @author avinesse
+ */
+public class ImageTools {
+    
+}
