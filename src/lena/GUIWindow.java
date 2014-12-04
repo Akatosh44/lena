6f36f1372ae3476c40929a59ec478d4663132737
akatosh <>
2014-12-04 11:00:11 +0100

    New GUI with ImagePanel class need to add comments and javadoc need to do img save

--- src/lena/GUIWindow.java
+++ src/lena/GUIWindow.java
@@ -4,41 +4,106 @@
  */
 package lena;
 
-import java.awt.BorderLayout;
+
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
 import javax.swing.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.util.EventListener;
 
 
 /**
  *
  * @author jlangloi
  */
-public class GUIWindow extends JFrame{
+public class GUIWindow extends JFrame implements EventListener{
     
     private JPanel content;
+    private Image image;
+    private JLabel imgName;
+    private ImagePanel contenuImage;
+
     
     public GUIWindow(int w, int h){
+        super("VIEWER");
         this.setSize(w, h);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+       
+        
         content = new JPanel();
         content.setSize(w, h);
-        content.setLayout(new BorderLayout());
+        content.setLayout(null);
+        //content.setBackground(Color.red);
+        this.setContentPane(content);
+        contenuImage=new ImagePanel();
+        imgName=new JLabel("");
+        imgName.setBounds(0, 0, w, 15);
+        image=null;
+        content.add(contenuImage);
+        content.add(imgName);
+        
         JMenuBar menuBar=new JMenuBar();
+        
         JMenu menu = new JMenu("File");
         menu.setMnemonic(KeyEvent.VK_F);
+        
         JMenuItem openItem = new JMenuItem("Open (.pgm)",KeyEvent.VK_O);
         openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        openItem.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser dialogue;
+                dialogue = new JFileChooser(new File("."));
+               
+                if (dialogue.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+                    File fichier = dialogue.getSelectedFile();
+
+                    if (fichier!=null){
+                        setImage(PGMTools.readPGM(fichier.getAbsolutePath()));
+                        contenuImage.setImage(image);
+                        imgName.setText(image.getName());
+                        repaint();
+                    }
+                    
+                }
+            }
+        });
+     
         JMenuItem saveItem = new JMenuItem("Save (.pgm)",KeyEvent.VK_S);
         saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
         menu.add(openItem);
         menu.add(saveItem);
+        
         menuBar.add(menu);
-        content.add(menuBar, BorderLayout.PAGE_START);
-        this.setTitle("LENA Viewer");
-        this.setContentPane(content);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.setJMenuBar(menuBar);
+
         this.setVisible(true);
+        
     }
-    //TODO
+    
+  
+    public JPanel getContent() {
+        return content;
+    }
+
+    public void setContent(JPanel content) {
+        this.content = content;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+
     
 }
