385b306283144999063d423d8bd9d951f2596b5b
ebriand <ebriand@pc-b13-15.eleves.ecn>
2014-12-04 09:59:48 +0100

    Method differences added to ImageTools

--- src/lena/ImageTools.java
+++ src/lena/ImageTools.java
@@ -60,6 +60,45 @@ public class ImageTools {
            }
            return newImage;
        }
+       
+        /**
+        * Computes the difference of grayLevel between two imgs
+        * @param img1
+        * @param img2
+        * @return 
+        */
+       public static Image differences(Image img1,Image img2){
+           if(img1.getWidth() == img2.getWidth() 
+                   && img1.getHeight() == img1.getHeight()){
+
+               // The images have the same width and height
+               Image diff = new Image("DifferenceObtenue",
+                       img1.getWidth(),img1.getHeight());
+               ArrayList<Pixel> pixelsDiff = new ArrayList<>();
+               ArrayList<Pixel> pixelsImg1 = img1.getPixels();
+               ArrayList<Pixel> pixelsImg2 = img2.getPixels();
+
+               for(int j =0; j<pixelsImg1.size();j++){
+
+                   Pixel p = pixelsImg1.get(j);
+                   Pixel pix = pixelsImg2.get(j);
+                   // Computing the grayLevel
+                   int grayLevel = p.getLevel() - pix.getLevel();
+                   Pixel pixDiff = new Pixel(p.getX(),p.getY(),0);
+                   if(grayLevel>0){
+                       pixDiff.setLevel(grayLevel);
+                   }
+                   // Add the result pixel in the ArrayList
+                   pixelsDiff.add(pixDiff);               
+               }
+               diff.setPixels(pixelsDiff);
+               return diff;    
+           }
+           else {
+               // Images with differents height and/or widths
+               return new Image("TailleDifferentes");
+           }
+       }
         
         
     
